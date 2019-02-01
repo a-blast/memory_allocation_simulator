4f587b395219a061bf475dd9df78275c97d43fdc
Austin <austin@growatomata.com>
2019-02-01 11:33:01 -0700

    includes the required functions in the header file

--- FrameAllocator.h
+++ FrameAllocator.h
@@ -8,7 +8,11 @@
 // NO I/O FROM THIS CLASS
 class FrameAllocator {
 public:
-  FrameAllocator();
+  FrameAllocator(uint64_t numPageFrames);
+  bool Allocate(uint32_t count, std::vector<uint32_t> &page_frames);
+  bool Release(uint32_t count, std::vector<uint32_t> &page_frames);
+  uint32_t get_available() const;
+
 private:
   std::vector<uint8_t> memory;
 };
